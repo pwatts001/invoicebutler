1dacbd3e1191acc19c3fe8eda0996732e7407ba8
phil <phill_666@hotmail.com>
2015-09-24 14:30:35 +0100

    fixed attr_accessor error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,8 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
 
-  	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missingAAA.png"
+    attr_accessor :image
+  	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  		validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
