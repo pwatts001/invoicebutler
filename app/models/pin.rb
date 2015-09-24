f65e3f8d737abc6a6f573ae56f8695476115b097
phil <phill_666@hotmail.com>
2015-09-24 14:58:22 +0100

    fixed attr_accessor error6

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,10 +1,10 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
 
-		attr_accessor :image_file_name
-		attr_accessor :image_content_type
-		attr_accessor :image_file_size
-		attr_accessor :image_updated_at
+		# attr_accessor :image_file_name
+		# attr_accessor :image_content_type
+		# attr_accessor :image_file_size
+		# attr_accessor :image_updated_at
 
   	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  		validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
