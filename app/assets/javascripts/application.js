3fd9796af19f0bbd3292a28f1a8618c5fd761e16
phil <phill_666@hotmail.com>
2015-12-04 13:35:23 +0000

    repositioned javescript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,7 @@
 //= require_tree .
 
 
+//Fat Face sticky header
 function moveScroller() {
     var move = function() {
         var st = $(window).scrollTop();
@@ -43,3 +44,31 @@ function moveScroller() {
 }
 
 
+//Typing text on homepage
+    $(function(){
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
+
+
+
+    
